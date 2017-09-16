5721b25a38aab97f829dfaccef07eb506e4bf495
xjchong <xavier.chong.j@gmail.com>
2017-09-16 18:43:43 -0400

    beautify menu screens

--- app/src/main/java/ca/we_love_different_things/bubbletea/GameOver.java
+++ app/src/main/java/ca/we_love_different_things/bubbletea/GameOver.java
@@ -1,13 +1,10 @@
 package ca.we_love_different_things.bubbletea;
 
 import android.content.Intent;
-import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class GameOver extends AppCompatActivity {
 
@@ -19,7 +16,7 @@ public class GameOver extends AppCompatActivity {
         Intent intent = getIntent();
         String score = intent.getStringExtra(Controller.EXTRA_SCORE);
         TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(textView.getText().toString() + score);
+        textView.setText("Game over!\nYou scored:\n" + score);
     }
 
     public void button(View view){
